6c92db8831600c8b4bdeef60e33bd958c98b4436
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-06-15 12:28:40 +0500

    :rotating_light: fixed linters

--- app/controllers/v1/home_controller.rb
+++ app/controllers/v1/home_controller.rb
@@ -1,7 +1,8 @@
-class V1::HomeController < ApplicationController
-  def index
-    # @messages = Message.all
-    @messages = Message.find(Random.rand(1..5))
-    render json: @messages
+module V1
+  class HomeController < ApplicationController
+    def index
+      @messages = Message.find(Random.rand(1..5))
+      render json: @messages
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'static/index'
-  
+
   namespace :v1, defaults: { format: 'json' } do
     resources :home, only: [:index]
   end
-  
-  root "static#index" 
-  
-  get '*page', to: 'static#index', constraints: ->(req) do
-  !req.xhr? && req.format.html?
-  end
+
+  root 'static#index'
+
+  get '*page', to: 'static#index', constraints: lambda { |req|
+    !req.xhr? && req.format.html?
+  }
 end
